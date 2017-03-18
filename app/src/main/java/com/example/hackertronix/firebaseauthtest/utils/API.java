2b9c8661d109ac1dc438440612b2ac4f08f5daf7
Prateek Phoenix <leavethemails@gmail.com>
2017-03-18 12:49:58 +0530

    added api and utils

---
+++ app/src/main/java/com/example/hackertronix/firebaseauthtest/utils/API.java
@@ -0,0 +1,16 @@
+package com.example.hackertronix.firebaseauthtest.utils;
+
+/**
+ * Created by hackertronix on 18/03/17.
+ */
+
+public class API {
+
+    public static final String API_ENDPOINT= "http://unsplash.it/list";
+
+    public static final String WIDTH="500";
+    public static final String HEIGHT="750";
+
+
+
+}
